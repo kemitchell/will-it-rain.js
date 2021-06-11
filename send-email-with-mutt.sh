acea5bb27ccf81dec0355fb4d99fe1a63a646eae
Kyle E. Mitchell <kyle@kemitchell.com>
2021-06-11 16:37:00 -0700

    Update

--- send-email-with-mutt.sh
+++ send-email-with-mutt.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ ! hash will-it-rain &>/dev/null ]; then
-  npm i -g kemitchell/will-it-rain.js
-fi
-
-if will-it-rain; then
+if npm exec --yes -p kemitchell/will-it-rain -- will-it-rain; then
   mutt -s "Rain Today" "$your_email" <<< "NOAA forecasts rain today."
 fi
